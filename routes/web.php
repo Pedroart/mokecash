47ba5a525027f207e53ef022a4ae4cd0c6caa6e7
PedroArtDesk <pedroarteta@hotmail.es>
2025-06-09 19:20:13 -0500

    link home

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\TiendaController;
 use App\Http\Controllers\PersonaltiendaController;
