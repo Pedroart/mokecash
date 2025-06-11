9042bd669b40fd346e2a06fb929bf3d6ed938e50
Pedroart <pedroarteta@hotmail.es>
2025-06-11 11:44:04 -0500

    test

--- resources/views/consulta-financiamiento/etapas/ingreso.blade.php
+++ resources/views/consulta-financiamiento/etapas/ingreso.blade.php
@@ -93,7 +93,7 @@
         Generar Boleta
     </button>
 @else
-    <a href="{{ route('boletas.show', ['boleta' => $cotizacion->boleta() ??: 1]) }}" class="btn btn-secondary mt-3 float-left">
+    <a href="{{ route('boletas.show', ['boleta' => $cotizacion->boleta() ?: 1]) }}" class="btn btn-secondary mt-3 float-left">
         Ver Boleta Generada
     </a>
 @endif
