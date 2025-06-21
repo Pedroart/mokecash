061c55db012b8b6d22c787eb809be61cdc45b9cb
PedroArtDesk <pedroarteta@hotmail.es>
2025-06-21 08:53:28 -0500

    ordenamiento numero

--- resources/views/cotizacion/index.blade.php
+++ resources/views/cotizacion/index.blade.php
@@ -104,6 +104,9 @@
             language: {
                 url: "https://cdn.datatables.net/plug-ins/2.3.2/i18n/es-ES.json"
             },
+            columnDefs: [
+                { targets: 0, type: 'num' } // <-- Asegura que la columna 0 se trate como numérica
+            ]
         });
     });
     </script>
